faed8b1a871f4307b7b821af6496cc08df7643f1
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2015-03-19 12:46:42 +0100

    Main now prints date time

--- src/main/java/dk/statsbiblioteket/demo/Main.java
+++ src/main/java/dk/statsbiblioteket/demo/Main.java
@@ -1,9 +1,12 @@
 package dk.statsbiblioteket.demo;
 
+import java.util.Date;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("This is the Demo of the Git");
+        System.out.println(new Date());
         System.out.println("This is now real");
     }
 }
