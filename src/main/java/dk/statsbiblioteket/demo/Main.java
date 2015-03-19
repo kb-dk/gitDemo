b789ab63eec0193c876e4aafb3f791eaf17a08c0
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2015-03-19 13:05:56 +0100

    print nothing

--- src/main/java/dk/statsbiblioteket/demo/Main.java
+++ src/main/java/dk/statsbiblioteket/demo/Main.java
@@ -5,9 +5,5 @@ import java.util.Date;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("This is the Demo of the Git");
-        System.out.println(new Date(0));
-        System.out.println("This is now real");
-        System.out.println("Print all the lines!!!");
     }
 }
